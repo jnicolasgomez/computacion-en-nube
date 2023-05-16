04e062e9e5ee262b2de7ca836b29ac939dd5053d
Nicolas Gomez <jngm_20@hotmail.com>
2023-05-15 23:34:25 -0500

    add docker files

--- BookStore/backend/config/db.js
+++ BookStore/backend/config/db.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import colors from 'colors';
 
 const connectDB = async() =>{
-
     try{
         const conn = await mongoose.connect(process.env.URL_DB_CONNECTION,{
             useUnifiedTopology: true,
